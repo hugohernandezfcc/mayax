7db615b5eb2f234399d8b50b61d58c432ab8c392
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-10-17 18:43:26 -0500

    First commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/requestDemo', function () {
+
+	
+
+	$client = new GuzzleHttp\Client();
+   	$response = $client->get('https://eu21.chat-api.com/instance13554/messages?token=5c3yhxh90meww8g5');
+
+	echo date("Y-m-d H:i:s", 1534208250);
+ //   	echo "<pre>";
+ //   	print_r(json_decode($response->getBody()));
+	// echo "</pre>";
+	// echo "<br/>";
+
+	$messages = json_decode($response->getBody());
+	for ($i=0; $i < count($messages->messages); $i++) { 
+		$messages->messages[$i]->time = date("Y-m-d H:i:s", $messages->messages[$i]->time);
+	}
+	
+	dd($messages->messages);
+
+});
