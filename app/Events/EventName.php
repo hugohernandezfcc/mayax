e8e40ec11cdd587bee44fe89d8fc76634b952b2c
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-10-21 03:29:04 -0500

    brodcast working

---
+++ app/Events/EventName.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+
+
+class EventName implements ShouldBroadcast
+
+{
+    use SerializesModels;
+
+    public $data;
+
+    public function __construct()
+    {
+        $this->data = array(
+            'power'=> '10'
+        );
+    }
+
+    public function broadcastOn()
+    {
+        return ['test-channel'];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::group(['prefix' => 'messages'], function(){
 	
@@ -29,4 +29,22 @@ Route::group(['prefix' => 'messages'], function(){
 		]
 	);
 
+});
+
+
+Route::get('/', function() {
+    // this doesn't do anything other than to
+    // tell you to go to /fire
+    return "go to /fire";
+});
+
+Route::get('fire', function () {
+    // this fires the event
+    event(new App\Events\EventName());
+    return "event fired";
+});
+
+Route::get('test', function () {
+    // this checks for the event
+    return view('welcome');
 });
